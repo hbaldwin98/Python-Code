a50699bfa779880211b55da5a1621fb775ca0327
Hunter Baldwin <huntercbaldwin98@gmail.com>
2020-02-06 16:30:46 -0800

    Create firstProgram.py

---
+++ firstProgram.py
@@ -0,0 +1,23 @@
+# Hunter Baldwin
+# Python programming course educative.io
+
+
+def maximum(first, second):
+    if first > second:
+        return first
+    else:
+        return second
+
+
+def triple(s): return s * 3
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+
+print(triple(10))
+print(factorial(5))
+
